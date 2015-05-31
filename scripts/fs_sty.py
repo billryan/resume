9f76654237e7c24f35919fee7a8e473081e0b3de
billryan <yuanbin2014@gmail.com>
2015-05-31 17:52:28 +0800

    add fontawesome sty scripts

---
+++ scripts/fs_sty.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import re
+from sys import argv
+
+scripts, css = argv
+
+
+# filter icon name, ignore alias
+def fil_icname(line):
+    if re.search('^\.fa-.*:before {$', line):
+        ic_name = re.split("[.:]", line)[1][3:]
+        return ic_name
+
+
+def fil_iccode(line):
+    if re.search('^  content: .*;$', line):
+        ic_code = re.split("[\"]", line)[1][1:].upper()
+        return ic_code
+
+
+# turn icon name to Camel Case
+# forked from https://github.com/schischi-a/fontawesome-latex
+def camel_case(name):
+    ret = name.replace('-', ' ')
+    ret = ret.title()
+    ret = ret.replace(' ', '')
+    return ret
+
+
+def get_icons(fs_css):
+    icons = []
+    with open(fs_css, 'r') as fs_fp:
+        for line in fs_fp:
+            icon_name = fil_icname(line)
+            if icon_name is not None:
+                line = next(fs_fp)
+                icon_code = fil_iccode(line)
+                if icon_code is not None:
+                    tex_name = camel_case(icon_name)
+                    icons.append((icon_name, icon_code, tex_name))
+    return icons
+
+
+def output_sty(sty, icons):
+    with open(sty, 'a') as f:
+        for ic in icons:
+            prefix = "\expandafter\def\csname faicon@"
+            ic_name_head = prefix + ic[0] + "\endcsname"
+            ic_code_tex = "{\symbol{\"" + ic[2] + "}}  \\def\\fa" + ic[1]
+            ic_name_tail = " {{\FA\csname faicon@" + ic[0] + "\endcsname}}\n"
+            f.write(ic_name.ljust(63) + ic_code_tex.ljust(42) + ic_name_tail)
+    # tail
+    f.write("\n")
+    f.write("\\endinput\n")
+    f.write("\n\n")
+    f.write(r"%% end of file `fontawesome.sty`.")
+
+
+if __name__ == "__main__":
+    icons = get_icons(css)
+    output_sty("fontawesome.sty", icons)
---
+++ scripts/fs_sty.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+script_root=$(dirname $0)
+resume_root=$(dirname "${script_root}")
+temp_root='/tmp/fs_script'
+fs_src="${temp_root}/fontawesome"
+mkdir -p $fs_src
+cp "$script_root/fs_sty.py" "$temp_root"
+cp "$script_root/fontawesome_preamble.sty" "$temp_root/fontawesome.sty"
+
+git clone -b v4.3.0 --depth 1 https://github.com/FortAwesome/Font-Awesome.git $fs_src
+cp "${fs_src}/css/font-awesome.css" "${temp_root}/fontawesome.css"
+cp "${fs_src}/fonts/FontAwesome.otf" "${resume_root}/fonts/FontAwesome.otf"
+cd $temp_root
+./fs_sty.py "fontawesome.css"
+cp "fontawesome.sty" "${resume_root}"
