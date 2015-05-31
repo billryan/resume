534d797ea93c2b57054494b9beec5dc492973ae4
billryan <yuanbin2014@gmail.com>
2015-05-31 18:39:40 +0800

    update

--- scripts/fs_sty.sh
+++ scripts/fs_sty.sh
@@ -5,8 +5,8 @@ resume_root=$(dirname "${script_root}")
 temp_root='/tmp/fs_script'
 fs_src="${temp_root}/fontawesome"
 
-[ -r "$fs_src" ] && $(rm "$fs_src")
-[ -d "$fs_src" ] && $(rm -r "$fs_src")
+[ -r "$fs_src" ] && $(rm -rf "$fs_src")
+#[ -d "$fs_src" ] && $(rm -r "$fs_src")
 mkdir -p $fs_src
 
 cp "$script_root/fs_sty.py" "$temp_root"
@@ -15,6 +15,5 @@ cp "$script_root/fontawesome_preamble.sty" "$temp_root/fontawesome.sty"
 git clone -b v4.3.0 --depth 1 https://github.com/FortAwesome/Font-Awesome.git $fs_src
 cp "${fs_src}/css/font-awesome.css" "${temp_root}/fontawesome.css"
 cp "${fs_src}/fonts/FontAwesome.otf" "${resume_root}/fonts/FontAwesome.otf"
-cd $temp_root
-./fs_sty.py "fontawesome.css"
-cp "fontawesome.sty" "${resume_root}"
+python "${temp_root}/fs_sty.py" "${temp_root}/fontawesome.css"
+cp "${temp_root}/fontawesome.sty" "${resume_root}"
